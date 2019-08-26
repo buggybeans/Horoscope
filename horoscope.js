5533947c506d900f420c8c0ca657658e661b9824
Prakhar-Mangal <mangalprakhar07@gmail.com>
2019-08-26 21:48:08 +0530

    border

--- horoscope.js
+++ horoscope.js
@@ -11,7 +11,7 @@ function getHoroscope() {
             for (i = 0; i < res.data.feed.entry.length; i++){
               str+=`<div class="mx-auto  mt-3 border border-danger">
             <div class="hovereffect">
-                <img class="img-responsive" src="${res.data.feed.entry[i].gsx$link.$t}" style="height: 150px; width: 100%;" 
+                <img class="img-responsive" src="${res.data.feed.entry[i].gsx$link.$t}" style="height: 150px; width: 150px;" 
                 alt="">
                 <div class="overlay">
                    <h2>${res.data.feed.entry[i].gsx$name.$t} </h2>
